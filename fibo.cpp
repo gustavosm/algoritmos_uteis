4b9301095cfd8e5c9f4a92abf000fc1280b1ccfd
gustavosm <guto_silveira_@hotmail.com>
2015-07-05 12:41:15 -0300

    Create fibo.cpp

---
+++ fibo.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+
+
+using namespace std;
+typedef long long int lli;
+typedef pair<lli, lli> ii;
+
+//calcula em O(logn) o n-esimo numero de fibonacci % mod
+ii fast_doubling(lli n, lli mod) 
+{
+	if(n == 1) return ii(1, 1);
+	else if(n == 2) return ii(1, 2);
+	
+	ii aux = fast_doubling(n/2, mod);
+	ii ret;
+	
+	ret.first = (aux.first * (aux.second * 2 + mod - aux.first)) % mod;
+	ret.second = ((lli)pow(aux.first, 2) + (lli)pow(aux.second, 2)) % mod;
+	 
+	return (n & 1? ret : ii(ret.second, (ret.first+ret.second)%mod));
+}
+
+int main()
+{
+	cout << fast_doubling(50, 100000007).first << '\n';
+}
